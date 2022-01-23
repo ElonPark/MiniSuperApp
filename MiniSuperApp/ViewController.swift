8bdcad92884d669dbada92107f78d1d2fc1c9172
Elon Park <sungwoon.park92@gmail.com>
2022-01-24 01:58:34 +0900

    build: Initial commit :tada:

---
+++ MiniSuperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniSuperApp
+//
+//  Created by Elon on 2022/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# MiniSuperApp
+MiniSuperApp
+
+Using
+1. uber/RIBs
+2. uber/Needle
+3. daangn/KarrotFlex
+4. devxoul/Then
