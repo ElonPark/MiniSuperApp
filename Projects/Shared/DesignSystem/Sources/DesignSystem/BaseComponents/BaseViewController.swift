639ae2aa57f451927ad2f7b83f4dc5f68653e750
Elon Park <dev.sungwoon@gmail.com>
2022-08-07 20:51:00 +0900

    fix: BaseViewController 수정

--- Projects/Shared/DesignSystem/Sources/DesignSystem/BaseComponents/BaseViewController.swift
+++ Projects/Shared/DesignSystem/Sources/DesignSystem/BaseComponents/BaseViewController.swift
@@ -7,4 +7,20 @@
 
 import UIKit
 
-class BaseViewController: UIViewController {}
+open class BaseViewController: UIViewController {
+
+  // MARK: - Initialization & Deinitialization
+
+  public init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  @available(*, unavailable)
+  public required convenience init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  deinit {
+    print("deinit \(type(of: self))")
+  }
+}
