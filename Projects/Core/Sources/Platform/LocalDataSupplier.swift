e64d600c1ac3801dcdfed369fc46a79dbefc2e3f
Elon Park <dev.sungwoon@gmail.com>
2022-08-28 18:17:54 +0900

    refactor: LocalDataSupplier 리팩토링

--- Projects/Core/Sources/Platform/LocalDataSupplier.swift
+++ Projects/Core/Sources/Platform/LocalDataSupplier.swift
@@ -15,104 +15,32 @@ import Entity
 // MARK: - LocalDataSupplierProtocol
 
 public protocol LocalDataSupplierProtocol {
-  var isFirstLaunch: Bool { get set }
   var user: User? { get set }
-  var userObservable: Observable<User?> { get }
 }
 
 // MARK: - LocalDataSupplier
 
 public final class LocalDataSupplier: LocalDataSupplierProtocol {
 
-  // MARK: Properties
+  public typealias Property = DiskCacheContainer.Property
+  public typealias CodableProperty = DiskCacheContainer.CodableProperty
 
-  @Property(key: "isFirstLaunch", defaultValue: true)
-  public var isFirstLaunch: Bool
+  // MARK: Properties
 
   @CodableProperty(key: "user", defaultValue: nil)
   public var user: User?
-  public var userObservable: Observable<User?> { self.$user }
 
   // MARK: Private
 
-  @DelayedMutable
-  private static var container: UserDefaults
+  private let container: DiskCacheContainerLogic
 
   // MARK: Initializing
 
-  public init(container: UserDefaults) {
-    Self.container = container
-  }
-}
-
-// MARK: - LocalDataSupplier.Property
-
-extension LocalDataSupplier {
-  @propertyWrapper
-  public class Property<Value> {
-    let key: String
-    let defaultValue: Value
-    let relay: BehaviorRelay<Value>
-
-    var container: UserDefaults?
-    private var _container: UserDefaults {
-      self.container ?? LocalDataSupplier.container
-    }
-
-    public var wrappedValue: Value {
-      get {
-        self._container.object(forKey: self.key) as? Value ?? self.defaultValue
-      }
-      set {
-        if let optional = newValue as? AnyOptional, optional.isNil {
-          self._container.removeObject(forKey: self.key)
-        } else {
-          self._container.set(newValue, forKey: self.key)
-        }
-        self.relay.accept(newValue)
-      }
-    }
-
-    open var projectedValue: Observable<Value> { self.relay.asObservable() }
-
-    public init(
-      key: String,
-      defaultValue: Value,
-      container: UserDefaults? = nil
-    ) {
-      self.key = key
-      self.defaultValue = defaultValue
-      self.container = container
-      self.relay = BehaviorRelay(value: self.defaultValue)
-    }
-  }
-}
-
-// MARK: - LocalDataSupplier.CodableProperty
-
-extension LocalDataSupplier {
-
-  @propertyWrapper
-  public final class CodableProperty<Value: Codable>: Property<Value> {
-
-    private var _container: UserDefaults {
-      self.container ?? LocalDataSupplier.container
-    }
-
-    override public var wrappedValue: Value {
-      get {
-        self._container.codable(for: self.key) ?? self.defaultValue
-      }
-      set {
-        if let optional = newValue as? AnyOptional, optional.isNil {
-          self._container.removeObject(forKey: self.key)
-        } else {
-          self._container.setCodable(value: newValue, for: self.key)
-        }
-        self.relay.accept(newValue)
-      }
-    }
-
-    override public var projectedValue: Observable<Value> { self.relay.asObservable() }
+  public init(
+    userDefaults: UserDefaults,
+    container: DiskCacheContainerLogic
+  ) {
+    self.container = container
+    self.container.setContainer(userDefaults)
   }
 }
