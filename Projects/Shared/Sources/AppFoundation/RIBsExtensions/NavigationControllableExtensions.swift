4fd7d361bee85321c3268e30943a086d6fdffbda
Elon Park <dev.sungwoon@gmail.com>
2022-10-02 18:29:21 +0900

    feat: add NavigationControllable

---
+++ Projects/Shared/Sources/AppFoundation/RIBsExtensions/NavigationControllableExtensions.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationControllable.swift
+//  
+//
+//  Created by elon on 2022/10/02.
+//
+
+import UIKit
+
+import RIBs
+
+extension UINavigationController: ViewControllable {
+
+  public var uiviewController: UIViewController { return self }
+
+  public convenience init(root: ViewControllable) {
+    self.init(rootViewController: root.uiviewController)
+  }
+}
